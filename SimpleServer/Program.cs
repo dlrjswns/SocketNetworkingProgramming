60286831b04d714a093d2fcde93c8fd663ac7870
dlrjswns <leekyunjun@gmail.com>
2022-10-04 14:46:00 +0900

    Thread를 이용하여 하나 이상의 Client의 요청시 불가했던 점 해결

--- SimpleServer/Program.cs
+++ SimpleServer/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace SimpleServer
 {
@@ -33,24 +34,55 @@ namespace SimpleServer
             {
                 Console.WriteLine("Server: waiting connection request");
                 Socket client = server.Accept();
-                Console.WriteLine("Server: client accepted");
+                ClientHandler cHandler = new ClientHandler(client);
+                Thread t= new Thread(new ThreadStart(cHandler.chat));
+                t.Start();
+                Console.WriteLine("Server: client accepted"); 
 
-                while(true)
-                {
-                    byte[] recvBytes = new byte[1024];
-                    int nRecv = client.Receive(recvBytes);
-                    if (nRecv <= 0) // 상대방이 종료함
-                    {
-                        client.Shutdown(SocketShutdown.Both);
-                        client.Close();
-                        break;
-                    }
-                    string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv); // 한글패치, Byte배열 0~nRecv까지 -> txt
-                    Console.WriteLine(client.RemoteEndPoint.ToString() + ":" + txt);
-                    byte[] sendByte = Encoding.UTF8.GetBytes("서버:" + txt);
-                    client.Send(sendByte);
+                //while(true)
+                //{
+                //    byte[] recvBytes = new byte[1024];
+                 //   int nRecv = client.Receive(recvBytes);
+                 //   if (nRecv <= 0) // 상대방이 종료함
+                 //   {
+                 //       client.Shutdown(SocketShutdown.Both);
+                 //       client.Close();
+                 //       break;
+                 //   }
+                 //   string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv); // 한글패치, Byte배열 0~nRecv까지 -> txt
+                 //   Console.WriteLine(client.RemoteEndPoint.ToString() + ":" + txt);
+                 //   byte[] sendByte = Encoding.UTF8.GetBytes("서버:" + txt);
+                 //   client.Send(sendByte);
                 }           
             }
+        }
+    }
+}
+
+class ClientHandler
+{
+    Socket client = null;
+    public ClientHandler (Socket socket)
+    {
+        this.client = socket;
+    }
+
+    public void chat()
+    {
+        while(true)
+        {
+            byte[] recvBytes = new byte[1024];
+            int nRecv = client.Receive(recvBytes);
+            if(nRecv <= 0)
+            {
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+                return;
+            }
+            string txt = Encoding.UTF8.GetString(recvBytes, 0, nRecv);
+            Console.WriteLine(client.RemoteEndPoint.ToString() + ":" + txt);
+            byte[] sendBytes = Encoding.UTF8.GetBytes("서버:" + txt);
+            client.Send(sendBytes);
         }
     }
 }
---
+++ Thread/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(threadFunc);
+            t.IsBackground = true; // 
+            t.Start();
+            // 메인스레드가 이외의 스레드보다 먼저 죽는 상황은 바람직하지않음
+            // 위와 같은 경우를 위한 코드, IsBackground, Join
+            t.Join();
+
+            ThreadParam param = new ThreadParam();
+            param.value1 = 10;
+            param.calue2 = 20;
+            t.Start(param);
+        }
+
+        static void threadFunc()
+        {
+            Console.WriteLine("60초 후에 프로그램 종료");
+            Thread.Sleep(1000 * 60);
+            Console.WriteLine("스레드 종료!");
+        }
+        // t.Start(10) 이런식으로 전달하여 threadFunc로 전달 가능 이때 인자의 타입은 반드시 object이므로 
+        // 추후에 다시 타입캐스팅해줘야함
+        static void threadFunc(object initialValue)
+        {
+            // int intValue = (int)initialValue;
+            // Console.WriteLine(intValue);
+
+
+            ThreadParam intValue = (ThreadParam)initialValue;
+            Console.WriteLine("{0}, {1}", intValue.value1, intValue.calue2);
+        }
+
+        class ThreadParam
+        {
+            public int value1;
+            public int calue2;
+        }
+
+        class ThreadWithParam
+        {
+            public int value1;
+            public int value2;
+            public ThreadWithParam(int val1, int val2)
+            {
+                this.value1 = val1;
+                this.value2 = val2;
+            }
+
+            static void threadFunc()
+            {
+                Console.WriteLine("");
+            }
+        }
+
+    }
+}
